9d00c1ade39c36aa6e8064083eca66a6ca40ce44
xiaoxunyao <huchengyea@163.com>
2022-10-06 12:09:01 +0800

    fix: add vector test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,7 +1,107 @@
 import { describe, expect, it } from 'vitest'
+import { Vector } from '../src'
 
-describe('should', () => {
-  it('exported', () => {
-    expect(1).toEqual(1)
+class NDimensional<Dimensions extends number[]> extends Vector<
+	Dimensions,
+	number
+> {
+  lastSet?: number
+  lastCoords?: Dimensions
+  get(...coords: Dimensions) {
+    const sum = coords.reduce((partialSum, a) => partialSum + a, 0)
+    return sum
+  }
+
+  set(v: number, ...coords: Dimensions) {
+    this.lastSet = v
+    this.lastCoords = coords
+  }
+}
+
+describe('vector', () => {
+  describe('1 Dimension', () => {
+    it('should get', () => {
+      const a = new NDimensional(10)
+      expect(a[0]).toStrictEqual(0)
+      expect(a[1]).toStrictEqual(1)
+      expect(a[5]).toStrictEqual(5)
+      expect(a[9]).toStrictEqual(9)
+
+      // Out of bounds
+      expect(a[10]).toStrictEqual(undefined)
+    })
+    it('should set', () => {
+      const a = new NDimensional(10)
+
+      a[1] = 10
+      expect(a.lastCoords).toStrictEqual([1])
+      expect(a.lastSet).toStrictEqual(10)
+
+      a[9] = 999
+      expect(a.lastCoords).toStrictEqual([9])
+      expect(a.lastSet).toStrictEqual(999)
+    })
+  })
+  describe('2 Dimensions', () => {
+    it('should get', () => {
+      const a = new NDimensional(128, 32)
+      expect(a[0][0]).toStrictEqual(0)
+      expect(a[1][0]).toStrictEqual(1)
+      expect(a[0][1]).toStrictEqual(1)
+      expect(a[5][4]).toStrictEqual(9)
+      expect(a[127][1]).toStrictEqual(128)
+
+      // Out of bounds
+      expect(a[128]).toStrictEqual(undefined)
+      expect(a[127][32]).toStrictEqual(undefined)
+    })
+    it('should set', () => {
+      const a = new NDimensional(128, 32)
+
+      a[4][10] = 5
+      expect(a.lastCoords).toStrictEqual([4, 10])
+      expect(a.lastSet).toStrictEqual(5)
+
+      a[9][0] = 999
+      expect(a.lastCoords).toStrictEqual([9, 0])
+      expect(a.lastSet).toStrictEqual(999)
+    })
+    it('should return same instance for SubVector', () => {
+      const a = new NDimensional(128, 32)
+      expect(a[0] === a[0]).toStrictEqual(true)
+      expect(a[0] !== a[1]).toStrictEqual(true)
+    })
+  })
+
+  describe('3 Dimensions', () => {
+    it('should get', () => {
+      const a = new NDimensional(128, 32, 5)
+      expect(a[0][0][0]).toStrictEqual(0)
+      expect(a[1][0][0]).toStrictEqual(1)
+      expect(a[0][1][0]).toStrictEqual(1)
+      expect(a[5][4][1]).toStrictEqual(10)
+      expect(a[127][1][4]).toStrictEqual(132)
+
+      // Out of bounds
+      expect(a[128]).toStrictEqual(undefined)
+      expect(a[127][32]).toStrictEqual(undefined)
+      expect(a[127][31][6]).toStrictEqual(undefined)
+    })
+    it('should set', () => {
+      const a = new NDimensional(128, 32, 5)
+
+      a[4][10][3] = 5
+      expect(a.lastCoords).toStrictEqual([4, 10, 3])
+      expect(a.lastSet).toStrictEqual(5)
+
+      a[9][0][3] = 999
+      expect(a.lastCoords).toStrictEqual([9, 0, 3])
+      expect(a.lastSet).toStrictEqual(999)
+    })
+    it('should return same instance for SubVector', () => {
+      const a = new NDimensional(128, 32, 1)
+      expect(a[0][0] === a[0][0]).toStrictEqual(true)
+      expect(a[0][0] !== a[0][1]).toStrictEqual(true)
+    })
   })
 })
